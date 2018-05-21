b4de6e48abd2b3c246ff2ac5288a1343dc69c821
froz-ferre <alexej.ferre@gmail.com>
2018-05-21 22:29:58 +0300

    init chat component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './shared/chat/chat.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
