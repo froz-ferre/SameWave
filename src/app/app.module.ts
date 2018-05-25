11fe237f7194c696d7d175830a47d6c7191b02de
froz-ferre <alexej.ferre@gmail.com>
2018-05-25 12:22:37 +0300

    shared start structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
+;
+import { MenuComponent } from './shared/menu/menu.component'import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ChatComponent } from './shared/chat/chat.component';
+import { 404Component } from './shared/404/404.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    404Component, MenuComponent
   ],
   imports: [
     BrowserModule,
