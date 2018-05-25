95ffc64776870d41b377d3909cea65240a18feb7
froz-ferre <alexej.ferre@gmail.com>
2018-05-25 15:19:49 +0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-;
-import { MenuComponent } from './shared/menu/menu.component'import { BrowserModule } from '@angular/platform-browser';
+import { MenuComponent } from './shared/menu/menu.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ChatComponent } from './shared/chat/chat.component';
-import { 404Component } from './shared/404/404.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
-    404Component, MenuComponent
+    MenuComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
